c36a48c150415dfe8208f61a51134040472f17b0
nleitefaria <nleitefaria@gmail.com>
2019-10-22 22:56:29 +0100

    INIT

---
+++ src/main/java/com/mycompany/hrkapp4fe/IndexController.java
@@ -0,0 +1,24 @@
+package com.mycompany.hrkapp4fe;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name = "index")
+@RequestScoped
+public class IndexController {
+	
+	private String message;
+
+    public IndexController(){
+        message = "Hello";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
